mitya57/gnome-panel

commit 9bc6d3cd8518b81a905a08c343c2607be9340056

don't use deprecated GtkSymbolicColor
Code for color shading copied from mutter:
https://git.gnome.org/browse/mutter/tree/src/ui/theme.c

applets/clock/Makefile.am
@@ -58,6 +58,7 @@ CLOCK_CPPFLAGS =						\
 
 CLOCK_LDADD =						\
 	../../libpanel-applet/libpanel-applet-4.la	\
+	../../gnome-panel/libpanel-util/libpanel-util.la	\
 	$(CLOCK_LIBS)					\
 	$(CLOCK_EDS_LIBS)					\
 	$(LIBPANEL_APPLET_LIBS)				\

applets/clock/clock-map.c
@@ -8,6 +8,8 @@
 #include <gtk/gtk.h>
 #include <math.h>
 
+#include <gnome-panel/libpanel-util/panel-color.h>
+
 #include "clock.h"
 #include "clock-map.h"
 #include "clock-sunpos.h"
@@ -227,24 +229,19 @@ static gboolean
 clock_map_draw (GtkWidget *this, cairo_t *cr)
 {
         ClockMapPrivate *priv = PRIVATE (this);
-	GtkStyleContext *context;
-	GdkRGBA  color;
-        GtkSymbolicColor *symbolic, *lighter_symbolic;
+        GtkStyleContext *context;
+        GdkRGBA  color;
         int width, height;
 
         context = gtk_widget_get_style_context (this);
         gtk_style_context_get_color (context, GTK_STATE_FLAG_ACTIVE, &color);
 
         /* Color for the outline. We want a light version of the active color */
-        symbolic = gtk_symbolic_color_new_literal (&color);
-        lighter_symbolic = gtk_symbolic_color_new_shade (symbolic, 3);
-        gtk_symbolic_color_resolve (lighter_symbolic, NULL, &color);
-        gtk_symbolic_color_unref (symbolic);
-        gtk_symbolic_color_unref (lighter_symbolic);
+        gtk_style_shade (&color, &color, 3);
 
-	if (!priv->shadow_map_pixbuf) {
+        if (!priv->shadow_map_pixbuf) {
                 g_warning ("Needed to refresh the map in draw event.");
-		clock_map_refresh (CLOCK_MAP (this));
+                clock_map_refresh (CLOCK_MAP (this));
         }
 
         width = gdk_pixbuf_get_width (priv->shadow_map_pixbuf);
@@ -260,7 +257,7 @@ clock_map_draw (GtkWidget *this, cairo_t *cr)
         cairo_set_line_width (cr, 1.0);
         cairo_stroke (cr);
 
-	return FALSE;
+        return FALSE;
 }
 
 static void

gnome-panel/libpanel-util/Makefile.am
@@ -51,6 +51,8 @@ libpanel_util_la_SOURCES =		\
 	panel-cleanup.h			\
 	panel-dconf.c			\
 	panel-dconf.h			\
+	panel-color.c			\
+	panel-color.h			\
 	panel-error.c			\
 	panel-error.h			\
 	panel-glib.c			\

gnome-panel/libpanel-util/panel-color.c
@@ -1,0 +1,198 @@
+#include "panel-color.h"
+
+/**
+ * rgb_to_hls:
+ * @r: on input, red; on output, hue
+ * @g: on input, green; on output, lightness
+ * @b: on input, blue; on output, saturation
+ *
+ * Converts a red/green/blue triplet to a hue/lightness/saturation triplet.
+ */
+static void
+rgb_to_hls (gdouble *r, gdouble *g, gdouble *b)
+{
+	gdouble min;
+	gdouble max;
+	gdouble red;
+	gdouble green;
+	gdouble blue;
+	gdouble h, l, s;
+	gdouble delta;
+
+	red = *r;
+	green = *g;
+	blue = *b;
+
+	if (red > green) {
+		if (red > blue)
+			max = red;
+		else
+			max = blue;
+
+		if (green < blue)
+			min = green;
+		else
+			min = blue;
+	} else {
+		if (green > blue)
+			max = green;
+		else
+			max = blue;
+
+		if (red < blue)
+			min = red;
+		else
+			min = blue;
+	}
+
+	l = (max + min) / 2;
+	s = 0;
+	h = 0;
+
+	if (max != min) {
+		if (l <= 0.5)
+			s = (max - min) / (max + min);
+		else
+			s = (max - min) / (2 - max - min);
+
+		delta = max -min;
+		if (red == max)
+			h = (green - blue) / delta;
+		else if (green == max)
+			h = 2 + (blue - red) / delta;
+		else if (blue == max)
+			h = 4 + (red - green) / delta;
+
+		h *= 60;
+		if (h < 0.0)
+			h += 360;
+	}
+
+	*r = h;
+	*g = l;
+	*b = s;
+}
+
+/**
+ * hls_to_rgb:
+ * @h: on input, hue; on output, red
+ * @l: on input, lightness; on output, green
+ * @s: on input, saturation; on output, blue
+ *
+ * Converts a hue/lightness/saturation triplet to a red/green/blue triplet.
+ */
+static void
+hls_to_rgb (gdouble *h, gdouble *l, gdouble *s)
+{
+	gdouble hue;
+	gdouble lightness;
+	gdouble saturation;
+	gdouble m1, m2;
+	gdouble r, g, b;
+
+	lightness = *l;
+	saturation = *s;
+
+	if (lightness <= 0.5)
+		m2 = lightness * (1 + saturation);
+	else
+		m2 = lightness + saturation - lightness * saturation;
+	m1 = 2 * lightness - m2;
+
+	if (saturation == 0) {
+		*h = lightness;
+		*l = lightness;
+		*s = lightness;
+	} else {
+		hue = *h + 120;
+		while (hue > 360)
+			hue -= 360;
+		while (hue < 0)
+			hue += 360;
+
+		if (hue < 60)
+			r = m1 + (m2 - m1) * hue / 60;
+		else if (hue < 180)
+			r = m2;
+		else if (hue < 240)
+			r = m1 + (m2 - m1) * (240 - hue) / 60;
+		else
+			r = m1;
+
+		hue = *h;
+		while (hue > 360)
+			hue -= 360;
+		while (hue < 0)
+			hue += 360;
+
+		if (hue < 60)
+			g = m1 + (m2 - m1) * hue / 60;
+		else if (hue < 180)
+			g = m2;
+		else if (hue < 240)
+			g = m1 + (m2 - m1) * (240 - hue) / 60;
+		else
+			g = m1;
+
+		hue = *h - 120;
+		while (hue > 360)
+			hue -= 360;
+		while (hue < 0)
+			hue += 360;
+
+		if (hue < 60)
+			b = m1 + (m2 - m1) * hue / 60;
+		else if (hue < 180)
+			b = m2;
+		else if (hue < 240)
+			b = m1 + (m2 - m1) * (240 - hue) / 60;
+		else
+			b = m1;
+
+		*h = r;
+		*l = g;
+		*s = b;
+	}
+}
+
+/**
+ * gtk_style_shade:
+ * @a: the starting colour
+ * @b: (out): the resulting colour
+ * @k: amount to scale lightness and saturation by
+ *
+ * Takes a colour "a", scales the lightness and saturation by a certain amount,
+ * and sets "b" to the resulting colour.
+ * gtkstyle.c cut-and-pastage.
+ */ 
+void
+gtk_style_shade (GdkRGBA *a, GdkRGBA *b, gdouble k)
+{
+	gdouble red;
+	gdouble green;
+	gdouble blue;
+
+	red = a->red;
+	green = a->green;
+	blue = a->blue;
+
+	rgb_to_hls (&red, &green, &blue);
+
+	green *= k;
+	if (green > 1.0)
+		green = 1.0;
+	else if (green < 0.0)
+		green = 0.0;
+
+  	blue *= k;
+  	if (blue > 1.0)
+  		blue = 1.0;
+  	else if (blue < 0.0)
+  		blue = 0.0;
+
+	hls_to_rgb (&red, &green, &blue);
+
+	b->red = red;
+	b->green = green;
+	b->blue = blue;
+}

gnome-panel/libpanel-util/panel-color.h
@@ -1,0 +1,12 @@
+#ifndef PANEL_COLOR_H
+#define PANEL_COLOR_H
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+void gtk_style_shade (GdkRGBA *a, GdkRGBA *b, gdouble k);
+
+G_END_DECLS
+
+#endif

gnome-panel/panel-frame.c
@@ -23,9 +23,9 @@
  */
 
 #include <config.h>
+#include <libpanel-util/panel-color.h>
 
 #include "panel-frame.h"
-
 #include "panel-typebuiltins.h"
 
 G_DEFINE_TYPE (PanelFrame, panel_frame, GTK_TYPE_BIN)
@@ -165,45 +165,35 @@ panel_frame_draw (GtkWidget     *widget,
                   PanelFrameEdge edges)
 {
         PanelFrame       *frame = (PanelFrame *) widget;
-	GtkStyleContext  *context;
-	GtkStateFlags     state;
+        GtkStyleContext  *context;
+        GtkStateFlags     state;
         GdkRGBA           bg, dark, light;
-        GtkSymbolicColor *c1, *c2;
-	int               x, y, width, height;
+        int               x, y, width, height;
         GtkBorder         padding;
 
-	if (edges == PANEL_EDGE_NONE)
-		return;
+        if (edges == PANEL_EDGE_NONE)
+                return;
 
-	context = gtk_widget_get_style_context (widget);
-	state = gtk_widget_get_state_flags (widget);
+        context = gtk_widget_get_style_context (widget);
+        state = gtk_widget_get_state_flags (widget);
         width = gtk_widget_get_allocated_width (widget);
         height = gtk_widget_get_allocated_height (widget);
 
         gtk_style_context_get_background_color (context, state, &bg);
 
-        c1 = gtk_symbolic_color_new_literal (&bg);
-
-        c2 = gtk_symbolic_color_new_shade (c1, 0.7);
-        gtk_symbolic_color_resolve (c2, NULL, &dark);
-        gtk_symbolic_color_unref (c2);
-
-        c2 = gtk_symbolic_color_new_shade (c1, 1.3);
-        gtk_symbolic_color_resolve (c2, NULL, &light);
-        gtk_symbolic_color_unref (c2);
-
-        gtk_symbolic_color_unref (c1);
+        gtk_style_shade (&bg, &dark, 0.7);
+        gtk_style_shade (&bg, &light, 1.3);
 
         gtk_style_context_get_padding (context, state, &padding);
 
-	/* Copied from gtk_default_draw_shadow() */
+        /* Copied from gtk_default_draw_shadow() */
 
         x = y = 0;
 
         cairo_set_line_width (cr, 1);
 
-	if (frame->edges & PANEL_EDGE_BOTTOM && padding.bottom > 0) {
-		if (padding.bottom > 1) {
+        if (frame->edges & PANEL_EDGE_BOTTOM && padding.bottom > 0) {
+                if (padding.bottom > 1) {
                         gdk_cairo_set_source_rgba (cr, &dark);
                         cairo_move_to (cr, x + .5, y + height - 2 + .5);
                         cairo_line_to (cr, x + width - 1 - .5, y + height - 2 + .5);
@@ -213,16 +203,16 @@ panel_frame_draw (GtkWidget     *widget,
                         cairo_move_to (cr, x + .5, y + height - 1 - .5);
                         cairo_line_to (cr, x + width - 1 - .5, y + height - 1 - .5);
                         cairo_stroke (cr);
-		} else {
-			gdk_cairo_set_source_rgba (cr, &dark);
+                } else {
+                        gdk_cairo_set_source_rgba (cr, &dark);
                         cairo_move_to (cr, x + .5, y + height - 1 - .5);
                         cairo_line_to (cr, x + width - 1 - .5, y + height - 1 - .5);
                         cairo_stroke (cr);
                 }
-	}
+        }
 
-	if (frame->edges & PANEL_EDGE_RIGHT && padding.right > 0) {
-		if (padding.right > 1) {
+        if (frame->edges & PANEL_EDGE_RIGHT && padding.right > 0) {
+                if (padding.right > 1) {
                         gdk_cairo_set_source_rgba (cr, &dark);
                         cairo_move_to (cr, x + width - 2 - .5, y + .5);
                         cairo_line_to (cr, x + width - 2 - .5, y + height - 1 - .5);
@@ -238,35 +228,35 @@ panel_frame_draw (GtkWidget     *widget,
                         cairo_line_to (cr, x + width - 1 - .5, y + height - 1 - .5);
                         cairo_stroke (cr);
                 }
-	}
+        }
 
-	if (frame->edges & PANEL_EDGE_TOP && padding.top > 0) {
+        if (frame->edges & PANEL_EDGE_TOP && padding.top > 0) {
                 gdk_cairo_set_source_rgba (cr, &light);
                 cairo_move_to (cr, x + .5, y + .5);
                 cairo_line_to (cr, x + width - 1 - .5, y + .5);
                 cairo_stroke (cr);
 
-		if (padding.right > 1) {
+                if (padding.right > 1) {
                         gdk_cairo_set_source_rgba (cr, &bg);
                         cairo_move_to (cr, x + .5, y + 1 + .5);
                         cairo_line_to (cr, x + width - 1 - .5, y + 1 + .5);
                         cairo_stroke (cr);
                 }
-	}
+        }
 
-	if (frame->edges & PANEL_EDGE_LEFT && padding.left > 0) {
+        if (frame->edges & PANEL_EDGE_LEFT && padding.left > 0) {
                 gdk_cairo_set_source_rgba (cr, &light);
                 cairo_move_to (cr, x + .5, y + .5);
                 cairo_line_to (cr, x + .5, y + height - 1 - .5);
                 cairo_stroke (cr);
 
-		if (padding.left > 1) {
+                if (padding.left > 1) {
                       gdk_cairo_set_source_rgba (cr, &bg);
                       cairo_move_to (cr, x + 1 + .5, y + .5);
                       cairo_line_to (cr, x + 1 + .5, y + height - 1 - .5);
                       cairo_stroke (cr);
                 }
-	}
+        }
 }
 
 static gboolean

commit e599b046d9bbfab261ae9f2d5266def22b8a06f9

This partially fixes 'make distcheck'
 * With this fix 'make distcheck' will continue
   a little further before it fails.
 * The error that this commit fixes was introduced
   by commit: 9bc6d3cd8518b81a905a08c343c2607be9340056

applets/clock/Makefile.am
@@ -52,6 +52,8 @@ CLOCK_CPPFLAGS =						\
 	$(CLOCK_EDS_CFLAGS)					\
 	-I$(srcdir)/../../libpanel-applet			\
 	-I$(top_builddir)/libpanel-applet			\
+	-I$(srcdir)/../../gnome-panel               \
+	-I$(top_builddir)/gnome-panel               \
 	-DGNOMELOCALEDIR=\""$(prefix)/$(DATADIRNAME)/locale"\"	\
 	-DCLOCK_EDS_ICONDIR="\"$(CLOCK_EDS_ICONDIR)\""		\
 	-DGWEATHER_I_KNOW_THIS_IS_UNSTABLE

applets/clock/clock-map.c
@@ -8,7 +8,7 @@
 #include <gtk/gtk.h>
 #include <math.h>
 
-#include <gnome-panel/libpanel-util/panel-color.h>
+#include <libpanel-util/panel-color.h>
 
 #include "clock.h"
 #include "clock-map.h"